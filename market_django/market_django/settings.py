5b989781846561581b45a140716fd903d4d21cb3
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-03-09 01:49:42 +0300

    Project NN4 version 1.3

--- market_django/market_django/settings.py
+++ market_django/market_django/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp1',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
--- market_django/market_django/urls.py
+++ market_django/market_django/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp1.views import index_page
+from main.views import index_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
