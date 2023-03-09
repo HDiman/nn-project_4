705bb0ada258cea1365d87731a839395b0b23ea8
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-03-09 23:58:11 +0300

    Project NN4 version 1.4

--- market_django/main/views.py
+++ market_django/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def index_page(request):
+def index(request):
     return render(request, 'index.html')
