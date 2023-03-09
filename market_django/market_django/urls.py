1c6fead1e37c8eb46c98e31561fe515283b75a4e
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-03-10 00:25:44 +0300

    Project NN4 version 1.6

--- market_django/market_django/urls.py
+++ market_django/market_django/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from main.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
     path('', include('main.urls'))
 ]
 
