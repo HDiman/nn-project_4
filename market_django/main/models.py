5b97f60f72c6827cfc56174de83be17d2c1567a8
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-03-11 11:53:18 +0300

    Project NN4 version 1.10 (Сделаны три вебстраницы)

--- market_django/main/models.py
+++ market_django/main/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField('Название', max_length=50)
+    task = models.TextField('Описание')
+
+    def __str__(self):
+        return self.title
+
+
 
