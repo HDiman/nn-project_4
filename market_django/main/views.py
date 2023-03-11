634631a6c217d462c81517a616c626df2afb6027
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-03-11 22:15:18 +0300

    Project NN4 version 1.12 (выводим текст базы данных на странице)

--- market_django/main/views.py
+++ market_django/main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from .models import Task
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.all()
+    # tasks = Task.objects.order_by('title')
+    # tasks = Task.objects.order_by('-id')[:1]
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
 
 
 def about(request):
