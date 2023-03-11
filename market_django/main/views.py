f31b606953ac8601a5258382fdfd910cffb6dd20
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-03-12 01:26:48 +0300

    Project NN4 version 1.14 (Проект закончен)

--- market_django/main/views.py
+++ market_django/main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 def index(request):
@@ -9,7 +10,21 @@ def index(request):
     return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
 
 def task(request):
-    return render(request, 'main/task.html')
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = "Форма была неверной"
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error,
+    }
+    return render(request, 'main/task.html', context)
 
 def about(request):
     return render(request, 'main/about.html')
